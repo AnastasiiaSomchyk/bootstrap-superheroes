886d5fdb1027621c90bff55715ef4cc7ee1290bf
AnastasiiaSomchyk <34519885+AnastasiiaSomchyk@users.noreply.github.com>
2018-04-03 16:17:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-function executeThisCodeIfXHRFails() {
-  // NO ES6
-  console.log("An error occurred while transferring");
-}
 
-function executeThisCodeAfterFileLoaded() {
-  // NO ES6
-  const data = JSON.parse(this.responseText);
-
-  // BUILD YOUR DOMSTRING HERE
-}
-
-var myRequest = new XMLHttpRequest();
-myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
-myRequest.addEventListener("error", executeThisCodeIfXHRFails);
-myRequest.open("GET", "bd/jobs.json");
-myRequest.send();
