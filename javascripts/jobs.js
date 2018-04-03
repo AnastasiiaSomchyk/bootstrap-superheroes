1a27b3553efbca88a4b6c787662225217a32e729
Anastasiia Somchyk <e4nastya@gmail.com>
2018-04-03 16:13:41 -0500

    xhr request jobs

--- javascripts/jobs.js
+++ javascripts/jobs.js
@@ -3,3 +3,34 @@ console.log("jobs");
 const printToDom = (domString, divId) => {
   document.getElementById(divId).innerHTML = domString;
 };
+
+const buildDomString = jobsArray => {
+  let domString = "";
+  jobsArray.forEach(job => {
+    domString += `<div class="jobs-type">`;
+
+    domString += `<h1>${job.title}</h1>`;
+
+    domString += `</div>`;
+  });
+  printToDom(domString, "work");
+};
+
+function executeThisCodeIfXHRFails() {
+  console.log("An error occurred ");
+}
+
+function executeThisCodeAfterFileLoaded() {
+  const data = JSON.parse(this.responseText);
+
+  buildDomString(data.jobs); // BUILD YOUR DOMSTRING HERE
+}
+
+const startApplication = () => {
+  var myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+  myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+  myRequest.open("GET", "../db/jobs.json");
+  myRequest.send();
+};
+startApplication();
